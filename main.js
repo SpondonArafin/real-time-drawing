85184125a33a42262d24680e11431d2429cf4cde
SpondonArafin <82678481+SpondonArafin@users.noreply.github.com>
2021-09-30 19:41:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
+
+function setup() {
+video = createCapture(VIDEO);
+video.size(550,550);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+    
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+
+}
+
+function modelLoaded() {
+    console.log('Model loaded!');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+         console.log(results);
+         noseX = results[0].pose.nose.x;
+         noseY = results[0].pose.nose.y;
+         console.log("noseX = " + noseX +" noseY = " + noseY);
+         leftWristX = results[0].pose.leftWrist.x;
+         rightWristX = results[0].pose.rightWrist.x;
+         difference = floor(leftWristX-rightWristX);
+         console.log("rightWristX = " + rightWristX +" leftWristX = " + leftWristX);
+    }
+}
+
+
+
+function draw() {
+     background('#3529b3');
+     stroke("#000000");
+     fill("#292f59");
+square(noseX,noseY,difference);
+
+document.getElementById("square-side").innerHTML = "Side of the square is " + difference; 
+}
+
+
